53b51606ffbabf19592de0e5b398c45186450b1e
francoleon08 <francoleon284@gmail.com>
2024-09-30 19:38:01 -0300

    init

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Base image for Python application
+FROM python:3.12-alpine
+
+# Create a working directory within the container
+WORKDIR /app
+
+# Copy requirements.txt file
+COPY requirements.txt .
+
+# Install dependencies listed in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy your application code
+COPY src ./src
+
+EXPOSE 80
+
+# Command to run the application (replace "main:app" with your actual entry point)
+CMD ["uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "80"]
---
+++ src/main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+import random
+
+app = FastAPI()
+
+class Response(BaseModel):
+    titulo: str
+    imagenfondo: str
+    resumen: str
+    precio: int
+    personas: str
+
+likes = {
+    "texto": "Homero Simpson, Marge Simpson y Bart Simpson",
+}
+
+@app.get("/")
+def get_random_movie():
+    pelicula = generate_random_movie()
+    
+    response = Response(
+        titulo = pelicula["title"],
+        imagenfondo = f"https://image.tmdb.org/t/p/w500{pelicula['backdrop_path']}",
+        resumen = pelicula["overview"],
+        precio = get_precio()["precio"],
+        personas = get_likes()["texto"]
+    )
+    
+    return response
+    
+
+def generate_random_movie():
+    url_tmdb = "https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=62e9afa9b26ec1658e4f7c572663a19b"
+
+    headers = {
+        "accept": "application/json"
+    }
+
+    response = requests.get(url_tmdb, headers=headers)
+    peliculas = response.json()["results"]
+
+    numero_azar = random.randint(1, 10)
+
+    pelicula = peliculas[numero_azar]
+
+    return pelicula
+
+def get_likes():
+    return likes
+
+def get_precio():
+    url = "https://preciotawd.onrender.com/"
+    response = requests.get(url)
+    return response.json()
